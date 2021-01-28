fbf1f86b61181ff781506cccf6405ebcf4b16926
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-01-28 14:30:36 +0100

    chore(terraform): Add S3 using terraform

---
+++ terraform/s3.tf
@@ -0,0 +1,33 @@
+resource "aws_s3_bucket" "uploads" {
+  bucket        = "${var.prefix}-${var.name}-${var.env}"
+  acl           = "public-read"
+  force_destroy = true
+  versioning {
+    enabled = false
+  }
+
+  policy = jsonencode({
+    "Version" = "2012-10-17"
+    "Id"      = "Policy-public-read-1"
+    "Statement" = [
+      {
+        "Sid"    = "AllowPublicRead"
+        "Effect" = "Allow"
+        "Principal" = {
+          "AWS" = "*"
+        }
+        "Action"   = "s3:GetObject"
+        "Resource" = "arn:aws:s3:::${var.prefix}-${var.name}-${var.env}/*"
+      }
+    ]
+  })
+
+
+  tags = {
+    name    = "terraform bucket for data for the lageso"
+    project = "flsshygn"
+    type    = "storage"
+  }
+
+}
+
