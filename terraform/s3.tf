a8697162049a440bda50f079622bffe3360dc946
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-04-27 09:20:51 +0200

    enable CORS

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -22,7 +22,13 @@ resource "aws_s3_bucket" "uploads" {
     ]
   })
 
-
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
   tags = {
     name    = "terraform bucket for data for the lageso"
     project = "flsshygn"
